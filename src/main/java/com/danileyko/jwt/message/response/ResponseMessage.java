b049c2c717487ab264dc4362b68f55356387f396
romandanileyko <AsPiRaNt112358>
2022-04-09 15:09:13 +0700

    Exclude maven frontend plugin.

--- src/main/java/com/danileyko/jwt/message/response/ResponseMessage.java
+++ src/main/java/com/danileyko/jwt/message/response/ResponseMessage.java
@@ -2,7 +2,6 @@ package com.danileyko.jwt.message.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.hibernate.validator.constraints.SafeHtml;
 
 @Data
 @AllArgsConstructor
